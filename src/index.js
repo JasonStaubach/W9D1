e766985c411f75a95f95bfa99f89df6ce9e1b8c5
JasonStaubach <jbstaubach@gmail.com>
2023-06-26 14:05:56 -0400

    Moving

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log("Webpack is working!");
+import MovingObject from "./moving-object.js";
+
+window.MovingObject = MovingObject;
+
+// console.log(MovingObject([4,4],[10,10]))
+
+//   window.mo;
+
+
+//   console.log(mo);
+
+
+
+
+const mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 5,
+    color: "#00FF00"
+  });
--- src/moving-object.js
+++ src/moving-object.js
@@ -1,3 +1,9 @@
 export default class MovingObject{
+    constructor (options) {
+        this.pos = options["pos"];
+        this.vel = options["vel"];
+        this.radius = options["radius"];
+        this.color = options["color"];
+    }
     
 }
